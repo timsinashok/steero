938b0178164a261e271d3cd73cfb3b8d3e95a3f6
timsinashok <ashoktimsina147181@gmail.com>
2025-08-02 12:45:18 -0700

    initial sterro-ios app

---
+++ Steero-iOS/Steero-iOS/Steero_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Steero_iOSApp.swift
+//  Steero-iOS
+//
+//  Created by Ashok Timsina on 8/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Steero_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Steero-iOS/Steero-iOSTests/Steero_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Steero_iOSTests.swift
+//  Steero-iOSTests
+//
+//  Created by Ashok Timsina on 8/2/25.
+//
+
+import Testing
+
+struct Steero_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
